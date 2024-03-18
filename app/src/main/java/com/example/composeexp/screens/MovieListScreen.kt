4156639e2b6e0620e822e9609648837eccd7966f
Gurvinder Singh <gurvinderscs@gmail.com>
2024-03-18 15:12:31 +0530

    Progress bar added

--- app/src/main/java/com/example/composeexp/screens/MovieListScreen.kt
+++ app/src/main/java/com/example/composeexp/screens/MovieListScreen.kt
@@ -1,20 +1,26 @@
 package com.example.composeexp.screens
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -25,25 +31,39 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.example.composeexp.model.Result
 import com.example.composeexp.utils.AppConstant.IMAGE_URL
 import com.example.composeexp.viewModels.MovieViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun MovieListScreen() {
     val movieViewModel: MovieViewModel = viewModel()
-    val movieList: State<List<Result>> = movieViewModel.movieList.collectAsState()
+    val movieList: State<List<Result>?> = movieViewModel.movieList.collectAsState()
     val loader = remember { mutableStateOf(true) }
+    val coroutineScope = rememberCoroutineScope()
 
-    LazyColumn {
-        item(movieList.value) {
-            if (movieList.value.isNotEmpty()) {
-                movieList.value.forEach {
-                    MovieItem(result = it)
-                }
-
+    // Show progress bar if data is still loading
+    if (movieList.value == null || loader.value) {
+        ShowProgressBar(show = true)
+    } else {
+        LazyColumn {
+            items(movieList.value!!) { movie ->
+                MovieItem(result = movie)
             }
+        }
+    }
 
+    // Hide progress bar once data is loaded
+    LaunchedEffect(movieList.value) {
+        if (movieList.value != null && movieList.value!!.isNotEmpty()) {
+            coroutineScope.launch {
+                delay(2000L) // 3 seconds delay
+                loader.value = false
+            }
         }
     }
 }
+
+
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -67,9 +87,12 @@ fun MovieItem(result: Result) {
 
 @Composable
 fun ShowProgressBar(show: Boolean) {
-    if (show) {
-        CircularProgressIndicator(
-        )
-
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        if (show) {
+            CircularProgressIndicator()
+        }
     }
 }
